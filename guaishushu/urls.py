3e295d713c5ee6b4971f600defc964f299d01dea
Westown <julytwilight@msn.com>
2014-12-11 18:01:08 +0800

    restframeworkt t 4

--- guaishushu/urls.py
+++ guaishushu/urls.py
@@ -10,11 +10,16 @@ urlpatterns = patterns('',
     # url(r'^$', 'guaishushu.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
     url(r'^admin/', include(admin.site.urls)),
 
     # url(r'^', include('snippets.urls')),
     url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
