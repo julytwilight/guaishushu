125753bdf75f7e2ac71f9c24e5626e15af274fe6
Westown <julytwilight@msn.com>
2014-12-12 18:02:46 +0800

    dj

--- guaishushu/urls.py
+++ guaishushu/urls.py
@@ -9,17 +9,20 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'guaishushu.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.api_root),
 
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     url(r'^admin/', include(admin.site.urls)),
 
     # url(r'^', include('snippets.urls')),
-    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/$', views.SnippetList.as_view(), name="snippet-list"),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 
-    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/$', views.UserList.as_view(), name="user-list"),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
